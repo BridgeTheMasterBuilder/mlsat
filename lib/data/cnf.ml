7d5eb0648d5269865a3319ac435fbe22e7d1df22
Master Builder <gylfi@stebbi.is>
2023-11-12 22:48:33 +0000

    okay

--- lib/data/cnf.ml
+++ lib/data/cnf.ml
@@ -64,7 +64,22 @@ end = struct
     incr count;
     add !count
 
-  let remove_literal_from_clauses l cs m = Ok m
+  (* TODO return list of occurrences in order to update occurrence map *)
+  let remove_literal_from_clauses l cs m =
+    let exception Empty_clause of Clause.t in
+    try
+      Ok
+        (IntSet.fold
+           (fun c m' ->
+             match get c m' with
+             | None -> m'
+             | Some ls -> (
+                 let diff = Clause.remove l ls in
+                 match Clause.size diff with
+                 | 0 -> raise_notrace (Empty_clause ls)
+                 | _ -> IntMap.add c diff m'))
+           cs m)
+    with Empty_clause ls -> Error ls
 end
 
 module OccurrenceMap : sig
