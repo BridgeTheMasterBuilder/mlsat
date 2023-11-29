3c30ed473eefa6aed5522edcd40c30d5b403bd1d
Master Builder <gylfi@stebbi.is>
2023-11-29 19:05:35 +0000

    Cool

--- lib/data/cnf.ml
+++ lib/data/cnf.ml
@@ -14,15 +14,33 @@ module Literal = struct
 end
 
 module Clause = struct
-  include Literal.Set
+  (* include Literal.Set *)
 
-  let of_list ls = Iter.(of_list ls |> map Literal.of_int |> of_iter)
-  let size = cardinal
+  (* let of_list ls = Iter.(of_list ls |> map Literal.of_int |> of_iter) *)
+  (* let size = cardinal *)
+
+  (* let show c = *)
+  (*   "(" ^ fold (fun l s -> Printf.sprintf "%s%s " s (Literal.show l)) c "" ^ ")" *)
+
+  (* let to_set = Fun.id *)
+  include List
+
+  type t = Literal.t list
+
+  let add = add_nodup ~eq:Literal.equal
+  let choose = hd
+  let fold f c s = fold_left (Fun.flip f) s c
+  let of_list = sort_uniq ~cmp:Literal.compare
+  let remove l = remove ~eq:Literal.equal ~key:l
+  let size = length
 
   let show c =
-    "(" ^ fold (fun l s -> Printf.sprintf "%s%s " s (Literal.show l)) c "" ^ ")"
+    "("
+    ^ fold_left (fun s l -> Printf.sprintf "%s%s " s (Literal.show l)) "" c
+    ^ ")"
 
-  let to_set = Fun.id
+  let to_list = Fun.id
+  let to_set = Literal.Set.of_list
 end
 
 module ClauseMap = struct
@@ -325,7 +343,7 @@ let rewrite ({ current_decision_level = d; assignments = a; trail = t; _ } as f)
   { f' with current_decision_level = d + 1; assignments = a'; trail = t' }
 
 let analyze_conflict { current_decision_level = d; assignments = a; _ } clause =
-  let ls = Literal.Set.elements clause in
+  let ls = Clause.to_list clause in
   let rec recur q c history =
     match CCFQueue.take_front q with
     | None -> c
