e1f5a11e0f760a3ff7e3b1cb00ac3600a96428f8
Master Builder <gylfi@stebbi.is>
2023-12-03 13:09:40 +0000

    HUH?

--- lib/data/clause.ml
+++ lib/data/clause.ml
@@ -1,58 +1,55 @@
 include List
 
 type elt = Literal.t
-type t = { clause : elt list; assigned : Z.t }
+type t = { set : elt list; size : int; present : Z.t }
 
-let find_first_unassigned { clause; assigned } =
+let find_first_unset { set; present; _ } =
   let exception Found of Literal.t in
   try
     fold_left
       (fun a l ->
         let open Z in
-        if equal (logand a one) zero then raise_notrace (Found l)
-        else shift_right a 1)
-      assigned clause
+        if equal (a land one) zero then raise_notrace (Found l)
+        else shift_right_trunc a 1)
+      present set
     |> ignore;
     raise Not_found
   with Found l -> l
 
-let reify { clause; assigned; _ } =
+let reify { set; present; _ } =
   let open Z in
   fold_left
     (fun (c, a) l ->
-      let a' = shift_right a 1 in
-      if equal (logand a one) zero then (l :: c, a') else (c, a'))
-    ([], assigned) clause
+      let a' = shift_right_trunc a 1 in
+      if equal (a land one) zero then (l :: c, a') else (c, a'))
+    ([], present) set
   |> fst
 
-let add l { clause; assigned } =
-  let open Z in
-  match List.find_opt (fun l' -> l' = l) clause with
-  | None -> { clause = l :: clause; assigned = shift_left assigned 1 }
-  | Some _ -> { clause; assigned }
+let add l { set; present; size } =
+  match List.find_opt (fun l' -> l' = l) set with
+  | None -> { set = l :: set; size = size + 1; present = Z.(present lsl 1) }
+  | Some _ -> { set; present; size }
 
-let choose = find_first_unassigned
-let empty = { clause = empty; assigned = Z.zero }
+let choose = find_first_unset
+let empty = { set = empty; size = 0; present = Z.zero }
 let fold f c acc = reify c |> fold_left (Fun.flip f) acc
-let is_empty { clause; assigned } = Z.popcount assigned = length clause
-let mem l { clause; _ } = mem l clause
+let is_empty { size; present; _ } = Z.popcount present = size
+let mem l { set; _ } = mem l set
 
 let of_list ls =
-  let clause =
-    Iter.(of_list ls |> map Literal.of_int |> sort_uniq |> of_iter)
-  in
-  { clause; assigned = Z.zero }
+  let set = Iter.(of_list ls |> map Literal.of_int |> sort_uniq |> of_iter) in
+  { set; size = length set; present = Z.zero }
 
-let original { clause; _ } = { clause; assigned = Z.zero }
+let original { set; size; _ } = { set; size; present = Z.zero }
 
-let remove l { clause; assigned } =
+let remove l { set; size; present } =
   let idx =
-    List.find_idx (fun l' -> l' = l) clause |> Option.get_exn_or "IDX" |> fst
+    List.find_idx (fun l' -> l' = l) set |> Option.get_exn_or "IDX" |> fst
   in
-  let open Z in
-  { clause; assigned = logor assigned (shift_left one idx) }
+  { set; size; present = Z.(present lor (one lsl idx)) }
 
-let size { clause; assigned; _ } = length clause - Z.popcount assigned
+let singleton x = { set = [ x ]; size = 1; present = Z.zero }
+let size { size; present; _ } = size - Z.popcount present
 
 let show c =
   "("
--- lib/data/common.ml
+++ lib/data/common.ml
@@ -11,3 +11,69 @@
 
 module IntSet = Set.Make (Int)
 module IntMap = Map.Make (Int)
+
+(* module IntSet = struct *)
+(*   include List *)
+
+(*   type elt = int *)
+(*   type t = { set : elt list; size : int; present : Z.t } *)
+
+(*   let find_first_unset { set; present; _ } = *)
+(*     let exception Found of Literal.t in *)
+(*     try *)
+(*       fold_left *)
+(*         (fun a l -> *)
+(*           let open Z in *)
+(*           if equal (a land one) zero then raise_notrace (Found l) *)
+(*           else shift_right_trunc a 1) *)
+(*         present set *)
+(*       |> ignore; *)
+(*       raise Not_found *)
+(*     with Found l -> l *)
+
+(*   let reify { set; present; _ } = *)
+(*     let open Z in *)
+(*     fold_left *)
+(*       (fun (c, a) l -> *)
+(*         let a' = shift_right_trunc a 1 in *)
+(*         if equal (a land one) zero then (l :: c, a') else (c, a')) *)
+(*       ([], present) set *)
+(*     |> fst *)
+
+(*   let add l { set; present; size } = *)
+(*     match List.find_opt (fun l' -> l' = l) set with *)
+(*     | None -> { set = l :: set; size = size + 1; present = Z.(present lsl 1) } *)
+(*     | Some _ -> { set; present; size } *)
+
+(*   let choose = find_first_unset *)
+(*   let empty = { set = empty; size = 0; present = Z.zero } *)
+(*   let fold f c acc = reify c |> fold_left (Fun.flip f) acc *)
+(*   let is_empty { size; present; _ } = Z.popcount present = size *)
+(*   let mem l { set; _ } = mem l set *)
+
+(*   let of_list ls = *)
+(*     let set = Iter.(of_list ls |> map Literal.of_int |> sort_uniq |> of_iter) in *)
+(*     { set; size = length set; present = Z.zero } *)
+
+(*   let original { set; size; _ } = { set; size; present = Z.zero } *)
+
+(*   let remove l { set; size; present } = *)
+(*     let idx = *)
+(*       List.find_idx (fun l' -> l' = l) set |> Option.get_exn_or "IDX" |> fst *)
+(*     in *)
+(*     { set; size; present = Z.(present lor (one lsl idx)) } *)
+
+(*   let singleton x = { set = [ x ]; size = 1; present = Z.zero } *)
+(*   let size { size; present; _ } = size - Z.popcount present *)
+
+(*   let show c = *)
+(*     "(" *)
+(*     ^ fold_left *)
+(*         (fun s l -> Printf.sprintf "%s%s " s (Literal.show l)) *)
+(*         "" (reify c) *)
+(*     ^ ")" *)
+
+(*   let to_iter c = reify c |> to_iter *)
+(*   let to_list = reify *)
+(*   let to_set c = reify c |> Literal.Set.of_list *)
+(* end *)
