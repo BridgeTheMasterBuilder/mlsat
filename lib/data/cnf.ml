ee942064b304224a647546d4c61f3bf363a676f4
Master Builder <gylfi@stebbi.is>
2024-03-29 00:16:41 +0000

    cooolllll this srororokck

--- lib/data/cnf.ml
+++ lib/data/cnf.ml
@@ -3,7 +3,7 @@
 type formula = {
   clauses : Clause.Map.t;
   original : Clause.Map.t;
-  occur : Occurrence.Map.t;
+  (* occur : Occurrence.Map.t; *)
   frequency : Frequency.Map.t;
   unit_clauses : (int * Clause.t) CCFQueue.t;
   current_decision_level : int;
@@ -18,7 +18,7 @@ exception Conflict of Clause.t * formula
 let show
     ({
        clauses;
-       occur;
+       (* occur; *)
        frequency;
        current_decision_level;
        database;
@@ -31,8 +31,6 @@ let show
     "Clauses:\n\
      %s\n\
      Original:\n\
-     %s\n\
-     Literals:\n\
      %s\n\
      Frequency:\n\
      %s\n\
@@ -47,7 +45,7 @@ let show
      %s\n"
     (if Clause.Map.is_empty clauses then "()" else Clause.Map.show clauses)
     (if Clause.Map.is_empty clauses then "()" else Clause.Map.show f.original)
-    (if Occurrence.Map.is_empty occur then "()" else Occurrence.Map.show occur)
+    (* (if Occurrence.Map.is_empty occur then "()" else Occurrence.Map.show occur) *)
     (if Frequency.Map.is_empty frequency then "()"
      else Frequency.Map.show frequency)
     current_decision_level
@@ -66,7 +64,7 @@ let show
 let add_clause
     ({
        clauses;
-       occur;
+       (* occur; *)
        frequency;
        unit_clauses = uc;
        assignments = a;
@@ -75,7 +73,7 @@ let add_clause
      } as f) clause =
   let n = Clause.Map.size clauses in
   let clauses' = Clause.Map.add clause clauses in
-  let occur' = Clause.fold (fun l -> Occurrence.Map.add l n) clause occur in
+  (* let occur' = Clause.fold (fun l -> Occurrence.Map.add l n) clause occur in *)
   let frequency' =
     Frequency.Map.add_many
       (Clause.to_iter clause
@@ -83,7 +81,6 @@ let add_clause
       |> Clause.of_iter)
       frequency
   in
-  (* TODO need to pass in the assignments here to see which watchers are valid *)
   let watched_clause = WatchedClause.of_clause a clause n in
   let watchers', uc' =
     match WatchedClause.watched_literals watched_clause with
@@ -93,36 +90,16 @@ let add_clause
           uc )
     | None -> (watchers, CCFQueue.snoc uc (n, clause))
   in
-  (* TODO why does this break? *)
-  (* let uc' = *)
-  (*   let unassigned, _, satisfied = *)
-  (*     Clause.fold *)
-  (*       (fun l (unassigned', falsified', satisfied') -> *)
-  (*         match Assignment.Map.find_opt l a with *)
-  (*         | Some ass' -> *)
-  (*             let l' = Assignment.literal ass' in *)
-  (*             let falso = Literal.signum l <> Literal.signum l' in *)
-  (*             (unassigned', falsified' && falso, satisfied' || not falso) *)
-  (*         | None -> (Clause.add l unassigned', false, satisfied')) *)
-  (*       clause *)
-  (*       (Clause.empty, true, false) *)
-  (*   in *)
-  (*   if satisfied then uc' *)
-  (*   else if Clause.size unassigned = 1 then ( *)
-  (*     Logs.debug (fun m -> m "Clause %d is ready for unit propagation" n); *)
-  (*     CCFQueue.snoc uc' (n, clause)) *)
-  (*   else uc' *)
-  (* in *)
   {
     f with
     clauses = clauses';
-    occur = occur';
+    (* occur = occur'; *)
     frequency = frequency';
     unit_clauses = uc';
     watchers = watchers';
   }
 
-let add_learned_clauses ({ original; assignments = a; _ } as f) db =
+let add_learned_clauses ({ original; _ } as f) db =
   let f' =
     let open Iter in
     List.to_iter (List.rev db)
@@ -175,18 +152,15 @@ let analyze_conflict { current_decision_level = d; assignments = a; _ } clause =
 let assignments { assignments = a; _ } = Assignment.Map.assignments a
 let learned_clauses { database; _ } = database
 
-let backtrack
-    { current_decision_level = d; assignments = a; trail = t; database = db; _ }
-    learned_clause =
+let backtrack { assignments = a; trail = t; database = db; _ } learned_clause =
   let d' =
     let open Iter in
     Clause.to_iter learned_clause
     |> filter_map (fun l -> Assignment.Map.find_opt l a)
-    |> map Assignment.level (* TODO *)
+    |> map Assignment.level
     |> sort ~cmp:(fun d1 d2 -> -compare d1 d2)
     |> drop 1 |> max |> Option.value ~default:0
   in
-  let d' = 0 in
   let _, f' =
     if d' = 0 then List.last_opt t |> Option.get_exn_or "TRAIL"
     else List.find (fun (ass, _) -> Assignment.was_decided_on_level ass d') t
@@ -338,7 +312,7 @@ let of_list _v _c l =
       {
         clauses = Clause.Map.empty;
         original = Clause.Map.empty;
-        occur = Occurrence.Map.empty;
+        (* occur = Occurrence.Map.empty; *)
         frequency = Frequency.Map.empty;
         current_decision_level = 0;
         assignments = Assignment.Map.empty;
@@ -351,15 +325,16 @@ let of_list _v _c l =
   in
   { f with original = Clause.Map.copy f.clauses }
 
-let restart ({ trail = t; database = db; _ } as f) =
+let restart ({ trail = t; database = db; watchers; _ } as f) =
   if List.is_empty t then f
   else
-    add_learned_clauses
-      (snd
-         (List.last_opt t
-         |> Option.get_exn_or
-              "Attempt to backtrack without previous assignments."))
-      db
+    let f' =
+      snd
+        (List.last_opt t
+        |> Option.get_exn_or
+             "Attempt to backtrack without previous assignments.")
+    in
+    add_learned_clauses { f' with watchers } db
 
 let update_watchers l ({ clauses; assignments = a; watchers; _ } as f) =
   let update_watcher l (({ id; _ } : WatchedClause.t) as c)
@@ -381,10 +356,7 @@ let update_watchers l ({ clauses; assignments = a; watchers; _ } as f) =
         { f' with watchers = watchers' }
     | Unit { id; _ } ->
         Logs.debug (fun m -> m "Clause %d is ready for unit propagation" id);
-        {
-          f' with
-          unit_clauses = CCFQueue.snoc uc' (id, ls) (* watchers = watchers'; *);
-        }
+        { f' with unit_clauses = CCFQueue.snoc uc' (id, ls) }
     | Falsified { id; _ } ->
         Logs.debug (fun m -> m "Clause %d is falsified" id);
         raise_notrace (Conflict (ls, f))
@@ -409,81 +381,25 @@ let make_assignment l ass ({ frequency; assignments = a; trail = t; _ } as f) =
     Ok f'
   with Conflict (c, f) -> Error (c, f)
 
-(* let make_assignment l ass *)
-(*     ({ *)
-(*        clauses; *)
-(*        occur; *)
-(*        frequency; *)
-(*        assignments = a; *)
-(*        unit_clauses = uc; *)
-(*        trail = t; *)
-(*        _; *)
-(*      } as f) = *)
-(*   let exception Conflict of Clause.t * formula in *)
-(*   let a' = Assignment.Map.add l ass a in *)
-(*   let t' = (ass, f) :: t in *)
-(*   let f = { f with assignments = a'; trail = t' } in *)
-(*   Logs.debug (fun m -> m "Making assignment %s" (Assignment.show ass)); *)
-(*   try *)
-(*     let uc', f' = *)
-(*       match Occurrence.Map.find_opt (Literal.neg l) occur with *)
-(*       | Some cs -> *)
-(*           IntSet.fold *)
-(*             (fun c (uc', f') -> *)
-(*               let ls = Clause.Map.find c clauses in *)
-(*               let unassigned, falsified, satisfied = *)
-(*                 Clause.fold *)
-(*                   (fun l (unassigned', falsified', satisfied') -> *)
-(*                     match Assignment.Map.find_opt l a' with *)
-(*                     | Some ass' -> *)
-(*                         let l' = Assignment.literal ass' in *)
-(*                         let falso = Literal.signum l <> Literal.signum l' in *)
-(*                         ( unassigned', *)
-(*                           falsified' && falso, *)
-(*                           satisfied' || not falso ) *)
-(*                     | None -> (Clause.add l unassigned', false, satisfied')) *)
-(*                   ls *)
-(*                   (Clause.empty, true, false) *)
-(*               in *)
-(*               if satisfied then (uc', f') *)
-(*               else if falsified then ( *)
-(*                 Logs.debug (fun m -> m "Clause %d is falsified" c); *)
-(*                 raise_notrace (Conflict (ls, f))) *)
-(*               else if Clause.size unassigned = 1 then ( *)
-(*                 Logs.debug (fun m -> *)
-(*                     m "Clause %d is ready for unit propagation" c); *)
-(*                 (CCFQueue.snoc uc' (c, ls), f')) *)
-(*               else (uc', f')) *)
-(*             cs (uc, f) *)
-(*       | None -> (uc, f) *)
-(*     in *)
-(*     let frequency' = *)
-(*       Frequency.Map.remove_literal l frequency *)
-(*       |> Frequency.Map.remove_literal (Literal.neg l) *)
-(*     in *)
-(*     let f' = { f' with frequency = frequency'; unit_clauses = uc' } in *)
-(*     Ok f' *)
-(*   with Conflict (c, f) -> Error (c, f) *)
+(* let eliminate_pure_literals ({ occur; _ } as f) = *)
+(*   let f' = *)
+(*     let open Iter in *)
+(*     Occurrence.Map.to_iter occur *)
+(*     |> fold *)
+(*          (fun ({ occur; _ } as f') (l, _) -> *)
+(*            if Occurrence.Map.mem (Literal.neg l) occur then f' *)
+(*            else *)
+(*              let i = *)
+(*                Assignment.Implication *)
+(*                  { literal = l; implicant = Clause.empty; level = 0 } *)
+(*              in *)
+(*              make_assignment l i f' |> Result.get_exn) *)
+(*          f *)
+(*   in *)
+(*   { f' with trail = [] } *)
 
-let eliminate_pure_literals ({ occur; _ } as f) =
-  let f' =
-    let open Iter in
-    Occurrence.Map.to_iter occur
-    |> fold
-         (fun ({ occur; _ } as f') (l, _) ->
-           if Occurrence.Map.mem (Literal.neg l) occur then f'
-           else
-             let i =
-               Assignment.Implication
-                 { literal = l; implicant = Clause.empty; level = 0 }
-             in
-             make_assignment l i f' |> Result.get_exn)
-         f
-  in
-  { f' with trail = [] }
-
-let preprocess = eliminate_pure_literals
-(* let preprocess = Fun.id *)
+(* let preprocess = eliminate_pure_literals *)
+let preprocess f = { f with trail = [] }
 
 let rec unit_propagate ({ unit_clauses = ucs; assignments = a; _ } as f) =
   match CCFQueue.take_front ucs with
@@ -494,14 +410,6 @@ let rec unit_propagate ({ unit_clauses = ucs; assignments = a; _ } as f) =
         |> Iter.find_pred (fun l -> not (Assignment.Map.mem l a))
       with
       | Some l ->
-          let d' =
-            let open Iter in
-            Clause.remove l uc |> Clause.to_iter
-            |> map (fun l ->
-                   Assignment.Map.find_opt l a
-                   |> Option.map_or ~default:0 Assignment.level)
-            |> max |> Option.get_or ~default:0
-          in
           let d' = f.current_decision_level in
           let i =
             Assignment.Implication { literal = l; implicant = uc; level = d' }
@@ -513,7 +421,5 @@ let rec unit_propagate ({ unit_clauses = ucs; assignments = a; _ } as f) =
 let make_decision ({ current_decision_level = d; _ } as f) =
   let l = choose_literal f in
   let dec = Assignment.Decision { literal = l; level = d + 1 } in
-  (* let f' = make_assignment l dec f |> Result.get_exn in *)
-  (* { f' with current_decision_level = d + 1 } *)
   make_assignment l dec f
   |> Result.map (fun f' -> { f' with current_decision_level = d + 1 })
--- lib/data/watchedClause.ml
+++ lib/data/watchedClause.ml
@@ -7,8 +7,7 @@ module M = struct
     watchers : (Literal.t * Literal.t) option;
   }
 
-  let compare { clause = c1; id = id1; _ } { clause = c2; id = id2; _ } =
-    compare id1 id2
+  let compare { id = id1; _ } { id = id2; _ } = compare id1 id2
 end
 
 type t = M.t
@@ -43,12 +42,10 @@ end
 let fold f x { clause; _ } = Array.fold f x clause
 
 let of_clause a c id =
-  (* TODO assert clause is not empty? *)
   let open Iter in
   let clause_iter = Clause.to_iter c in
   let clause = clause_iter |> to_array in
   let size = Array.length clause in
-  (* let watchers = if size >= 2 then Some (clause.(0), clause.(1)) else None in *)
   let watchers =
     filter (fun l -> not (Assignment.is_false l a)) clause_iter
     |> take 2 |> to_list
@@ -64,105 +61,6 @@ type update_result =
   | Falsified of t
   | NoChange
 
-(* let update l a ({ clause; size; index; watchers; _ } as c) = *)
-(*   let open Iter in *)
-(*   let w1, w2 = *)
-(*     let w1, w2 = Option.get_exn_or "UPDATE" watchers in *)
-(*     if Literal.equal w1 l then (w1, w2) else (w2, w1) *)
-(*   in *)
-(*   let other_watcher_needs_updating = *)
-(*     match Assignment.Map.find_opt w2 a with *)
-(*     | Some ass *)
-(*       when let l' = Assignment.literal ass in *)
-(*            Literal.signum w2 <> Literal.signum l' -> *)
-(*         true *)
-(*     | _ -> false *)
-(*   in *)
-(*   if not other_watcher_needs_updating then NoChange *)
-(*   else ( *)
-(*     Logs.debug (fun m -> *)
-(*         m "%d (watching %s and %s - index %d): " c.id (Literal.show w1) *)
-(*           (Literal.show w2) index); *)
-(*     Array.iter *)
-(*       (fun l -> *)
-(*         Logs.debug (fun m -> *)
-(*             m "%s(%s) " (Literal.show l) *)
-(*               (Assignment.Map.find_opt l a *)
-(*               |> Option.map_or ~default:"_" (fun ass -> *)
-(*                      Literal.show (Assignment.literal ass))))) *)
-(*       c.clause; *)
-(*     let { index = index'; watcher1_change; watcher2_change; clause_falsified } = *)
-(*       0 -- (size - 1) *)
-(*       |> fold_while *)
-(*            (fun { *)
-(*                   index = index'; *)
-(*                   watcher1_change; *)
-(*                   watcher2_change; *)
-(*                   clause_falsified = falsified'; *)
-(*                   _; *)
-(*                 } _ -> *)
-(*              let index'' = (index' + 1) mod size in *)
-(*              let l' = clause.(index') in *)
-(*              match Assignment.Map.find_opt l' a with *)
-(*              | Some ass *)
-(*                when let l'' = Assignment.literal ass in *)
-(*                     Literal.signum l' <> Literal.signum l'' -> *)
-(*                  ( { *)
-(*                      index = index''; *)
-(*                      watcher1_change; *)
-(*                      watcher2_change; *)
-(*                      clause_falsified = falsified'; *)
-(*                    }, *)
-(*                    `Continue ) *)
-(*              | _ -> ( *)
-(*                  match watcher1_change with *)
-(*                  | None -> *)
-(*                      ( { *)
-(*                          index = index''; *)
-(*                          watcher1_change = *)
-(*                            (if Literal.equal w2 l' then None else Some l'); *)
-(*                          watcher2_change = None; *)
-(*                          clause_falsified = false; *)
-(*                        }, *)
-(*                        if other_watcher_needs_updating then `Continue else `Stop *)
-(*                      ) *)
-(*                  | Some l'' -> *)
-(*                      ( { *)
-(*                          index = index''; *)
-(*                          watcher1_change; *)
-(*                          watcher2_change = *)
-(*                            (if Literal.equal l' l'' then None else Some l'); *)
-(*                          clause_falsified = false; *)
-(*                        }, *)
-(*                        `Stop ))) *)
-(*            { *)
-(*              index; *)
-(*              watcher1_change = None; *)
-(*              watcher2_change = None; *)
-(*              clause_falsified = true; *)
-(*            } *)
-(*     in *)
-(*     if clause_falsified then Falsified c *)
-(*     else *)
-(*       match (watcher1_change, watcher2_change) with *)
-(*       | Some w1', Some w2' -> *)
-(*           let c' = { c with watchers = Some (w1', w2'); index = index' } in *)
-(*           WatcherChange (w1, w1', w2, w2', c') *)
-(*       | Some w1', None when not other_watcher_needs_updating -> *)
-(*           let c' = { c with watchers = Some (w1', w2); index = index' } in *)
-(*           WatcherChange (w1, w1', w2, w2, c') *)
-(*       | _ -> Unit c) *)
-
-(* TODO *)
-(* mlsat: [DEBUG] Updating watchers for clause 128:(-45 -12 11 13 37 46  ) *)
-(* mlsat: [DEBUG] 128 (watching -45 and -12 - index 2): *)
-(* mlsat: [DEBUG] -45(_) *)
-(* mlsat: [DEBUG] -12(12) *)
-(* mlsat: [DEBUG] 11(-11) *)
-(* mlsat: [DEBUG] 13(_) *)
-(* mlsat: [DEBUG] 37(_) *)
-(* mlsat: [DEBUG] 46(_) *)
-(* mlsat: [DEBUG] Clause 128 is ready for unit propagation *)
 let update l a ({ clause; size; index; watchers; _ } as c) =
   let open CCEither in
   let open Iter in
